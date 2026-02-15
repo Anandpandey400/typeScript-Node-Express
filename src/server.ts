2e16be488d616f449967cd8b244a7041df88dbc7
Anandpandey400 <pandeyanand026@gmail.com>
2026-02-15 12:39:25 +0530

    Finally Done ci/cd

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ app.use((req, _res, next) => {
 
 // health check
 app.get("/health", (_req: Request, res: Response) => {
-    res.send("Server is running heaalthy and checking again 🚀");
+    res.send("Server is running 🚀");
 });
 
 //  routes
