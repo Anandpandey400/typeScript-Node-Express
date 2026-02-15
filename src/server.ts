fd0aed9c6babd3015b224bec75211d6e3db873ce
Anandpandey400 <pandeyanand026@gmail.com>
2026-02-15 11:44:52 +0530

    Checking

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ app.use((req, _res, next) => {
 
 // health check
 app.get("/health", (_req: Request, res: Response) => {
-    res.send("Server is running heaalthy 🚀");
+    res.send("Server is running heaalthy and checking 🚀");
 });
 
 //  routes
